b62e9ac490f9f87c2ee165d642cd15d94ab35b1f
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2020-10-06 20:06:58 +0200

    refactor: Created code for Login Screen and Navigation.
    Code for the content of Login screen and Navigation is basically finished along with basic styles in CSS

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -3,8 +3,19 @@ import React, { Component } from "react";
 class NewQuestion extends Component {
     render() {
         return (
-            <div>
-                New Question
+            <div className='create-question'>
+                <h2>Create New Question</h2>
+                <h3>Would you rather...</h3>
+                <input
+                    type='text'
+                    placeholder='Option one text'
+                />
+                <h3>OR</h3>
+                <input
+                    type='text'
+                    placeholder='Option two text'
+                />
+                <button type='submit'>Submit</button>
             </div>
         );
     }
